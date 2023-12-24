ce5e3f3e73ef47e7e1527b15a38ca3a7d4955b3b
XJINE <>
2023-12-24 11:20:30 +0900

    Update to save settings.

--- Assets/Packages/PostProcessZipBuilders/Editor/PostProcessZipBuilder.cs
+++ Assets/Packages/PostProcessZipBuilders/Editor/PostProcessZipBuilder.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEditor;
 using UnityEngine;
 
@@ -17,13 +18,12 @@ namespace PostProcessZipBuilders
 
         public static bool ShowDialogWhenBuild = true;
 
-        public static RenameType RenameZip    = RenameType.None;
-        public static string     RenameFormat = "MMddHHmm";
-
-        public static bool Zip            = true;
-        public static bool Overwrite      = true;
-        public static bool IncludeBaseDir = true;
-        public static bool RemoveBaseDir  = false;
+        public static bool       Zip            = true;
+        public static bool       Overwrite      = true;
+        public static bool       IncludeBaseDir = true;
+        public static bool       RemoveBaseDir  = false;
+        public static RenameType RenameZip      = RenameType.None;
+        public static string     RenameFormat   = "MMddHHmm";
 
         public static bool RemoveStreamingAssets = false;
 
@@ -53,11 +53,43 @@ namespace PostProcessZipBuilders
 
             ShowTitle("General");
             ShowDialogWhenBuild = EditorGUILayout.Toggle("Show Dialog When Build", ShowDialogWhenBuild);
+
+            if (GUI.changed)
+            {
+                SaveSettings();
+            }
         }
 
         private static void ShowTitle(string title)
         {
             EditorGUILayout.LabelField("\u25A0 " + title, EditorStyles.boldLabel);
+        }
+
+        public static void SaveSettings()
+        {
+            const string baseName = nameof(PostProcessZipBuilder);
+            EditorUserSettings.SetConfigValue($"{baseName}.{nameof(ShowDialogWhenBuild)}",   ShowDialogWhenBuild  .ToString());
+            EditorUserSettings.SetConfigValue($"{baseName}.{nameof(Zip)}",                   Zip                  .ToString());
+            EditorUserSettings.SetConfigValue($"{baseName}.{nameof(Overwrite)}",             Overwrite            .ToString());
+            EditorUserSettings.SetConfigValue($"{baseName}.{nameof(IncludeBaseDir)}",        IncludeBaseDir       .ToString());
+            EditorUserSettings.SetConfigValue($"{baseName}.{nameof(RemoveBaseDir)}",         RemoveBaseDir        .ToString());
+            EditorUserSettings.SetConfigValue($"{baseName}.{nameof(RenameZip)}",             RenameZip            .ToString());
+            EditorUserSettings.SetConfigValue($"{baseName}.{nameof(RenameFormat)}",          RenameFormat         .ToString());
+            EditorUserSettings.SetConfigValue($"{baseName}.{nameof(RemoveStreamingAssets)}", RemoveStreamingAssets.ToString());
+        }
+
+        [InitializeOnLoadMethod]
+        public static void LoadSettings()
+        {
+            const string baseName = nameof(PostProcessZipBuilder);
+            bool.TryParse (EditorUserSettings.GetConfigValue($"{baseName}.{nameof(ShowDialogWhenBuild)}"),   out ShowDialogWhenBuild);
+            bool.TryParse (EditorUserSettings.GetConfigValue($"{baseName}.{nameof(Zip)}"),                   out Zip);
+            bool.TryParse (EditorUserSettings.GetConfigValue($"{baseName}.{nameof(Overwrite)}"),             out Overwrite);
+            bool.TryParse (EditorUserSettings.GetConfigValue($"{baseName}.{nameof(IncludeBaseDir)}"),        out IncludeBaseDir);
+            bool.TryParse (EditorUserSettings.GetConfigValue($"{baseName}.{nameof(RemoveBaseDir)}"),         out RemoveBaseDir);
+            Enum.TryParse (EditorUserSettings.GetConfigValue($"{baseName}.{nameof(RenameZip)}"),             out RenameZip);
+            RenameFormat = EditorUserSettings.GetConfigValue($"{baseName}.{nameof(RenameFormat)}");
+            bool.TryParse (EditorUserSettings.GetConfigValue($"{baseName}.{nameof(RemoveStreamingAssets)}"), out RemoveStreamingAssets); ;
         }
     }
 
